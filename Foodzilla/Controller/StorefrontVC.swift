0a343ba48618b9439bbd61600890ea0fd1c18895
Ironbatpug <molnarcsaba92@gmail.com>
2019-09-14 13:24:53 +0200

    initial commit

---
+++ Foodzilla/Controller/StorefrontVC.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Foodzilla
+//
+//  Created by Molnár Csaba on 2019. 09. 14..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class StorefrontVC: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    @IBAction func restoreBtnWasPressed(_ sender: Any) {
+    }
+    
+}
+
+extension StorefrontVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell
+    }
+}
